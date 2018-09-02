421fdf95252bced10669d497e8b4cb5b39edf07b
Gustavo Perenciolo <contatogustavo9@gmail.com>
2018-09-02 01:40:41 -0300

    First commit

---
+++ crud-node-postgres/server/config/database.js
@@ -0,0 +1,18 @@
+/********************************************************
+Database Connection Settings
+*********************************************************/
+
+exports.conString = "postgres://postgres:mypassword@db/crud-node";
+
+
+
+// module.exports = {
+//    query: function(text, values, cb) {
+//       pg.connect(function(err, client, done) {
+//         client.query(text, values, function(err, result) {
+//           done();
+//           cb(err, result);
+//         })
+//       });
+//    }
+// }
